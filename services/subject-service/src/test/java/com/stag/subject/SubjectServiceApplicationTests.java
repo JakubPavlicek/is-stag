a2977f5e0219b9acad8a79753e54340515d246b8
Jakub Pavlíček <kuba.cb@seznam.cz>
2025-07-08 14:03:36 +0200

    Add initial configuration for microservices architecture with config, discovery, and subject services

---
+++ services/subject-service/src/test/java/com/stag/subject/SubjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stag.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
