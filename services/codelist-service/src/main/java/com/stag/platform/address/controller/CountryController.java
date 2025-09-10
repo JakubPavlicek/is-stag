85d336f2a42b4865eb08b0185f897b956829b901
Jakub Pavlíček <kuba.cb@seznam.cz>
2025-09-10 16:04:36 +0200

    Implement GET /domains, GET /domains/{domain} endpoints; move @EnableMethodSecurity to the SecurityConfig; permit all requests to the /countries and /domains endpoints

--- services/codelist-service/src/main/java/com/stag/platform/address/controller/CountryController.java
+++ services/codelist-service/src/main/java/com/stag/platform/address/controller/CountryController.java
@@ -22,8 +22,8 @@ public class CountryController implements CountriesApi {
     private final CountryService countryService;
 
     // TODO:
-    //  Add:
-    //    GET /address (debounce effect) - (dorucovaci adresa)
+    //   Add:
+    //     GET /address (debounce effect) - (dorucovaci adresa)
 
     @Override
     public ResponseEntity<CountryListResponse> getCountries(String language) {
