9a66766b7c6f099390a234548b70be0016d6063d
Jakub Pavlíček <kuba.cb@seznam.cz>
2025-10-03 18:49:14 +0200

    Bump Spring Boot version to 4.0.0-M3

--- services/codelist-service/src/main/java/com/stag/platform/CodelistServiceApplication.java
+++ services/codelist-service/src/main/java/com/stag/platform/CodelistServiceApplication.java
@@ -2,13 +2,12 @@ package com.stag.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @EnableCaching
-@SpringBootApplication(exclude = { ElasticsearchRestClientAutoConfiguration.class })
+@SpringBootApplication
 public class CodelistServiceApplication {
 
     public static void main(String[] args) {
