da316c1d16716e68f982059188ad944cde370430
Jakub Pavlíček <kuba.cb@seznam.cz>
2026-03-19 16:47:10 +0100

    Add @rolldown/plugin-babel and refactor React compiler configuration in Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types="vitest" />
+import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import tanstackRouter from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
@@ -9,18 +10,13 @@ import { defineConfig } from 'vitest/config'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const ReactCompilerConfig = {
-  target: '19',
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
-      },
+    react(),
+    babel({
+      presets: [reactCompilerPreset({ target: '19' })],
     }),
     tailwindcss(),
   ],
